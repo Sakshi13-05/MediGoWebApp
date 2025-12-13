cf8fc6cd0e885501e38f4202e2b5dda0c81a7a2b
Sakshi13-05 <sakshichavan61116@gmail.com>
2025-12-13 13:59:51 +0530

    Updated API to production URL

--- frontend/my-app/src/utils/api.js
+++ frontend/my-app/src/utils/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 // You can configure API base URL (fallback is localhost)
-export const API_BASE = import.meta.env.VITE_API_BASE_URL || "http://localhost:5000";
+export const API_BASE = import.meta.env.VITE_API_BASE_URL || "https://medigo-api-j1d7.onrender.com";
 
 export const api = axios.create({
   baseURL: API_BASE, // All requests will automatically prefix this URL
