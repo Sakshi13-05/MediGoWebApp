617fe8c5c646cc8405fda93a4ce12c5d70d369e7
Sakshi13-05 <sakshichavan61116@gmail.com>
2025-12-14 08:01:44 +0530

    Comments Removed

--- frontend/my-app/src/utils/api.js
+++ frontend/my-app/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// ⚠️ HARDCODE THIS for the interview to ensure it works. 
-// Do not use localhost here.
+
+
 export const api = axios.create({
   baseURL: "https://medigo-api-j1d7.onrender.com", 
 });
