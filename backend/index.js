06b6448e799657795febf674d931a51c64d2aa9a
Sakshi13-05 <sakshichavan61116@gmail.com>
2026-03-08 18:50:15 +0530

    chore: update project branding and metadata

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import { MongoClient, ObjectId } from "mongodb";
+import { MongoClient} from "mongodb";
 import dotenv from "dotenv";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
