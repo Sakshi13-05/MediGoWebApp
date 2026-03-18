c59e2615b5992b3ef63025d09920af01ad361466
Sakshi13-05 <sakshichavan61116@gmail.com>
2026-03-18 20:37:39 +0530

    Route Handling

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,33 +1,33 @@
-import Otp from "../models/Otp";
-import User from "../models/User";
-import {sendEmail} from "../utils/mailSender";
+import Otp from "../models/Otp.js";
+import User from "../models/User.js";
+import { sendEmail } from "../utils/mailSender";
 
 // handles when back button is clicked
-export const requestOTP=async ()=>{
-    const {email}=req .body;
-    if(!email) return res. status(400).json({message: "email is required"});
-    // 1.gen otp
-    const otp=Math.floor(100000+ Math.random() *900000).toString();
-    try{
-        // save otp to db (upsert update if exist else create)
-        await Otp.findOneAndUpdate({email},{otp},{upsert:true,new:true});
-        //  Check if user already exists (to tell frontend whether to show full form or just login)
+export const requestOTP = async () => {
+  const { email } = req.body;
+  if (!email) return res.status(400).json({ message: "email is required" });
+  // 1.gen otp
+  const otp = Math.floor(100000 + Math.random() * 900000).toString();
+  try {
+    // save otp to db (upsert update if exist else create)
+    await Otp.findOneAndUpdate({ email }, { otp }, { upsert: true, new: true });
+    //  Check if user already exists (to tell frontend whether to show full form or just login)
     const user = await User.findOne({ email });
-    res.status(200).json({ 
-      success: true, 
-      isNewUser: !user, 
-      message: "OTP sent to email" 
+    res.status(200).json({
+      success: true,
+      isNewUser: !user,
+      message: "OTP sent to email"
     });
 
-    }catch(err){
-        res.status(500).json({success:false,message:error.message});
-        
-    }
+  } catch (err) {
+    res.status(500).json({ success: false, message: error.message });
+
+  }
 }
 
-export default verifyOTP=async()=>{
-     const { email, otp } = req.body;
-  
+export default verifyOTP = async () => {
+  const { email, otp } = req.body;
+
   const record = await Otp.findOne({ email, otp });
   if (!record) {
     return res.status(400).json({ success: false, message: "Invalid or expired OTP" });
